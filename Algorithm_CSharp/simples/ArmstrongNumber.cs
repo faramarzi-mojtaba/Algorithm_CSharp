36769531d6d503c4052cc678f832de663a4d315c
faramarzi-mojtaba <faramarzi.mojtaba@gmail.com>
2014-11-02 17:41:21 -0500

    Armstrong number algorithm was added

---
+++ Algorithm_CSharp/simples/ArmstrongNumber.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp.simples
+{
+    class ArmstrongNumber
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter a 3 digit number to find if its an Armstrong number:");
+                Int32 input = Int32.Parse(Console.ReadLine());
+                if (isArmstrong(input))
+                {
+                    Console.WriteLine("Number : " + input + " is an Armstrong number");
+                }
+                else
+                {
+                    Console.WriteLine("Number : " + input + " is not an Armstrong number");
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("armstrongNumber --> main "+exp.Message);
+            }
+            
+        }
+        public static bool isArmstrong(int input)
+        {
+            try
+            {
+                int original = input;
+                int result = 0;
+                while (input !=0)
+                {
+                    int reminder = input % 10;
+                    result += reminder * reminder * reminder;
+                    input /= 10;
+                }
+                if (original == result)
+                {
+                    return true;
+                }
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("Armstrong --> isArmstrong "+exp.Message);
+            }
+            return false;
+        }
+    }
+}
