1acbd36563a9d47632a0dc254be13c0674e215fc
faramarzi-mojtaba <faramarzi.mojtaba@gmail.com>
2014-11-07 00:58:23 -0500

    Greatest common divisor was added

---
+++ Algorithm_CSharp/simples/GratestCommonDivisor.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp.simples
+{
+    public class GratestCommonDivisor
+    {
+        public static void demo()
+        {
+            try
+            {
+                Console.WriteLine("Enter Your First Number:");
+                int p = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Your Second Number:");
+                int q = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Recursive Approach Result:");
+                Console.WriteLine(recursiveGcd(p, q));
+                Console.WriteLine("Non Recursive Approach Result:");
+                Console.WriteLine(nonRecursiveGcd(p, q));
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("GreatestCommonDivisor --> main " + e.Message);
+            }
+        }
+        public static int recursiveGcd(int p, int q)
+        {
+            if (q == 0) return p;
+            else return recursiveGcd(q, p % q);
+        }
+        public static int nonRecursiveGcd(int p, int q)
+        {
+            try
+            {
+                while (q != 0)
+                {
+                    int temp = q;
+                    q = p % q;
+                    p = temp;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("GratestCommonDivisor --> nonRecursiveGcd " + e.Message);
+            }
+            return p;
+        }
+    }
+
+}
---
+++ Algorithm_CSharp/simples/StringReverse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp.simples
+{
+    public class StringReverse
+    {
+        public static void demo()
+        {
+            try
+            {
+                Console.WriteLine("Please Enter Your String: ");
+                String str = Console.ReadLine();
+                Console.WriteLine("Recursive Approach Result:");
+                Console.WriteLine(recursiveReverse(str));
+                Console.WriteLine("Non Recursive Approach Result:");
+                Console.WriteLine(nonRecursiveReverse(str));
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("StringReverse --> main " + e.Message);
+            }
+        }
+        public static string recursiveReverse(string str)
+        {
+            if (str.Length < 2) return str;
+            else return recursiveReverse(str.Substring(1)) + str[0];
+        }
+        public static string nonRecursiveReverse(string str)
+        {
+            string result = "";
+            try
+            {
+                for (int i = str.Length - 1; i >= 0; i--)
+                {
+                    result += str[i];
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("StringReverse --> nonRecursiveReverse " + e.Message);
+            }
+            return result;
+        }
+    }
+}
