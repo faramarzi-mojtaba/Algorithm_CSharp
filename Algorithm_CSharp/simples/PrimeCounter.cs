c5726897a26d4320e37c7843b686848d71500e6f
faramarzi-mojtaba <faramarzi.mojtaba@gmail.com>
2014-11-06 21:14:57 -0500

    Prime Counter was added that was written based on Prime Class .

---
+++ Algorithm_CSharp/simples/PrimeCounter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp.simples
+{
+    public class PrimeCounter
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please Enter Your Number :");
+                int number = Int32.Parse(Console.ReadLine());
+                int counter = 0;
+                for (int i = 2; i < number; i++)
+                {
+                    if (Prime.isPrime(i))
+                        counter++;
+                }
+                Console.WriteLine("The number of Prime number less than " + number + " is : " + counter);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("PrimeCounter --> main " + e.Message);
+            }
+        }
+    }
+}
