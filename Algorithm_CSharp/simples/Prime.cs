a7217fe2ef779d7b29ab5ae29fd183daddff6b56
faramarzi-mojtaba <faramarzi.mojtaba@gmail.com>
2014-11-06 20:39:58 -0500

    Prime algorithm was added

---
+++ Algorithm_CSharp/simples/Prime.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp.simples
+{
+    public class Prime
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Your Number Please :");
+                int number = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Your Number Is Prime Number : " + isPrime(number));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Prime --> main " + e.Message);
+            }
+        }
+        public static bool isPrime(int number)
+        {
+            try
+            {
+                for (int i = 2; i * i < number; i++)
+                {
+                    if (number % i == 0)
+                        return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Prime --> isPrime " + e.Message);
+            }
+            return true;
+        }
+    }
+}
